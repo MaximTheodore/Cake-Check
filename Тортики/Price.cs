9abdd169a7a8d9e9eba4d1e0d5c3fe6d8515c99b
MaximTheodore <maximtarasenkov2005@mail.ru>
2022-10-28 16:31:20 +0300

    load

---
+++ Тортики/Price.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тортики
+{
+   
+    internal class Price
+    {
+        public string Name { get; set; }
+        public int price { get; set; }
+        public Price(string name, int prices)
+        {
+            Name = name;
+            price = prices;
+        }
+
+
+    }
+}
